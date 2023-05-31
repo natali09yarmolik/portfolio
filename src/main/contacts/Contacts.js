eff1104e78c7ddd51649d23502c0834e109ad0fc
natali09yarmolik <85163434+natali09yarmolik@users.noreply.github.com>
2023-05-31 14:29:22 +0300

    fix: update Contacts.js

--- src/main/contacts/Contacts.js
+++ src/main/contacts/Contacts.js
@@ -4,15 +4,14 @@ import style from '../../container.module.scss'
 import emailjs from "emailjs-com";
 
 export const Contacts=()=> {
-const sentEmail = (e) =>{
-    e.preventDefault()
-    emailjs.sendForm('VVmlYXN30tWg6SLEB', 'template_mikv1ru', e.target, 'VVmlYXN30tWg6SLEB')
-        .then((result) => {
-            window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior)
-        }, (error) => {
-            console.log(error.text);
-        });
-}
+    const sentEmail = (e) => {
+        e.preventDefault()
+        emailjs.sendForm('VVmlYXN30tWg6SLEB', 'template_mikv1ru', e.target, 'VVmlYXN30tWg6SLEB')
+            .then((result) => {
+            }, (error) => {
+            });
+        e.target.reset()
+    }
 
     return (
         <div id={'id-contacts'} className={s.contacts}>
@@ -21,14 +20,17 @@ const sentEmail = (e) =>{
                 <form className={s.contactsBlockForm} onSubmit={sentEmail}>
                     <input type={"text"}
                            name={'name'}
+                           required
                            placeholder={'Enter your name'}
                            className={s.contactsBlockInput}/>
-                    <input type={"number"}
-                           name={'phone'}
-                           placeholder={'Enter your phone'}
+                    <input type={"email"}
+                           name={'email'}
+                           required
+                           placeholder={'Enter your email'}
                            className={s.contactsBlockInput}/>
                     <textarea placeholder={'Enter your message'}
                               name={'message'}
+                              required
                               className={s.contactsBlockTextarea}></textarea>
                     <button type={'submit'}>Send</button>
                 </form>
